bd76fa015d9c6b3d85c70e38136f6bf5402c7b86
oday.shibli.99@gmail.com <58177069+Odayshibli@users.noreply.github.com>
2021-05-25 17:43:10 +0300

    Update cram.java
    update grundy to 2*N

--- src/cramgame/cram.java
+++ src/cramgame/cram.java
@@ -4,45 +4,65 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+package cramgame;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class cram {
-    static ArrayList<Integer> array;
+	
+	static	ArrayList<Integer> array =new ArrayList<Integer>();
+	public static int calculate(int n) {
+	array=new ArrayList<Integer>();
+		if (n==0|| n==1 || n==2  ) return 0;
+			if (n==3 || n==4 || n==5) return 1;
+		
+		for(int i=0;i <= n;i++) {
+			array.add(i,calculateGrundy(i) );;
+		}
+	
+		return array.get(n);
+		
+		
+	}
+	public static  int calculateGrundy (int n)
+	{
+		
+		//stop condition 
+		if (n==0|| n==1 || n==2  ) return 0;
+		if (n==3 || n==4 || n==5) return 1;
+		
+		//A set to calculate the mex of grundy numbers .
+		Set<Integer> Set = new HashSet<Integer>();  
+		
 
-    public static int calculateGrundy(int n) {
-        array = new ArrayList<Integer>();
-        if (n != 0 && n != 1) {
-            for(int i = 0; i <= n; ++i) {
-                array.add(i, calculate(i));
-            }
+		//array.set(2, 1);
+		
+				
+			for (int i=0 ; i<n/2 ;i++)
+				//the first player choose a square.
+				Set.add((array.get(n-i-3))^(array.get(i)));
+			    
 
-            return (Integer)array.get(n);
-        } 
-            return 0; 
-    }
-
-    public static int calculate(int n) {
-        if (n != 0 && n != 1) {
-            Set<Integer> Set = new HashSet<Integer>();
-
-            for(int i = 0; i < n / 2; ++i) {
-                Set.add((Integer)array.get(n - i - 2) ^ (Integer)array.get(i));
-            }
-
-            return calculateMex(Set);
-        }
-            return 0;
-    }
-
-    public static int calculateMex(Set<Integer> Set) {
-        int Mex;
-        for(Mex = 0; Set.contains(Mex); ++Mex) {
-        }
-        return Mex;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(calculateGrundy(100));
-    }
+		//return the mex of grundy numbers.
+		return (calculateMex(Set));
+		}
+	
+	public static int calculateMex(Set<Integer> Set)
+	{
+	    int Mex = 0;
+	    while (Set.contains(Mex))
+	        Mex++;
+	    return Mex;
+	}
+	   
+ public static void main(String[] args)
+ {
+	 System.out.println(calculate(10));
+	 System.out.println(calculate(7));
+	 
+ }
 }
-
 	
 
